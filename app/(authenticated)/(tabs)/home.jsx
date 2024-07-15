26754679df63d3de695de241df5a38f2d8781042
Wen Ni Lim <wenni1999@gmail.com>
2024-07-15 17:56:25 +0800

    custom dropdown is created

--- app/(authenticated)/(tabs)/home.jsx
+++ app/(authenticated)/(tabs)/home.jsx
@@ -1,11 +1,31 @@
-import { View, Text, Button } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import { Text, View } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import RoundBtn from "../../../components/RoundBtn";
+import { Ionicons } from "@expo/vector-icons";
 
 const Page = () => {
   const balance = 1280;
+  const [open, setOpen] = useState(false);
   const onAddMoney = () => {};
+  const toggleMore = () => {
+    console.log("toggle more");
+    setOpen((prev) => !prev);
+  };
+
+  const menuItems = [
+    { label: "Statement", icon: "cash" },
+    { label: "Converter", icon: "calculator" },
+    { label: "Background", icon: "image" },
+    { label: "Add new account", icon: "person-add" },
+  ];
+
+  const buttons = [
+    { icon: "add", text: "Add Money", onPress: onAddMoney },
+    { icon: "refresh", text: "Exchange" },
+    { icon: "list", text: "Details" },
+    { icon: "ellipsis-horizontal", text: "More", onPress: toggleMore },
+  ];
   return (
     <ScrollView className="bg-gray-100">
       <View className="m-20 items-center">
@@ -21,10 +41,32 @@ const Page = () => {
       </View>
 
       <View className="flex-row p-5 justify-between">
-        <RoundBtn icon="add" text="Add Money" onPress={onAddMoney} />
-        <RoundBtn icon="refresh" text="Exchange" />
-        <RoundBtn icon="list" text="Details" />
+        {buttons.map((btn, index) => (
+          <RoundBtn
+            key={index}
+            icon={btn.icon}
+            text={btn.text}
+            onPress={btn.onPress}
+          />
+        ))}
       </View>
+      {open && (
+        <View className="flex-row mx-5 justify-end">
+          <View className="flex-col bg-white px-3 w-fit rounded-lg shadow-lg">
+            {menuItems.map((item, index) => (
+              <React.Fragment key={index}>
+                <View className="flex-row items-center justify-between py-3">
+                  <Text className="text-lg font-medium w-60">{item.label}</Text>
+                  <Ionicons name={item.icon} size={24} color="black" />
+                </View>
+                {index < menuItems.length - 1 && (
+                  <View className="w-full h-0.5 bg-gray-100" />
+                )}
+              </React.Fragment>
+            ))}
+          </View>
+        </View>
+      )}
     </ScrollView>
   );
 };
